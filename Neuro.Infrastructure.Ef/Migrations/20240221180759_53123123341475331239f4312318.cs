c56d9a4a4ac4863c08ac59a8a4a87c0aae0c2ed9
Gathaus <rizamertyagci@gmail.com>
2024-02-21 21:08:22 +0300

    medicine target adjusment migrations

---
+++ Neuro.Infrastructure.Ef/Migrations/20240221180759_53123123341475331239f4312318.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Neuro.Infrastructure.Ef.Migrations
+{
+    /// <inheritdoc />
+    public partial class _53123123341475331239f4312318 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MedicineTarget",
+                table: "TargetGroups");
+
+            migrationBuilder.AddColumn<short>(
+                name: "MedicineTarget",
+                table: "UserTargets",
+                type: "smallint",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MedicineTarget",
+                table: "UserTargets");
+
+            migrationBuilder.AddColumn<short>(
+                name: "MedicineTarget",
+                table: "TargetGroups",
+                type: "smallint",
+                nullable: true);
+        }
+    }
+}
