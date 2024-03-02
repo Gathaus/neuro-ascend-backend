8fa2422b47c9bea9812637ba80a20e8d0bacc376
Gathaus <rizamertyagci@gmail.com>
2024-03-02 13:22:51 +0300

    MedicineTimeTakenDto updated

--- Neuro.Api/Controllers/v1/MedicineController.cs
+++ Neuro.Api/Controllers/v1/MedicineController.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
 using Neuro.Api.Models;
 using Neuro.Application.Base.Service;
 using Neuro.Application.Extensions;
+using Neuro.Application.Managers.Abstract;
 using Neuro.Domain.Entities;
+using Neuro.Domain.Entities.Enums;
 using Neuro.Domain.UnitOfWork;
 
 namespace Neuro.Api.Controllers.v1;
@@ -16,13 +19,16 @@ public class MedicineController : BaseController
 
     private readonly BaseBusinessService _baseService;
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IUserService _userService;
 
 
-    public MedicineController(BaseBusinessService baseService, IUnitOfWork unitOfWork)
+    public MedicineController(BaseBusinessService baseService, IUnitOfWork unitOfWork, IUserService userService)
     {
         _baseService = baseService;
         _unitOfWork = unitOfWork;
+        _userService = userService;
     }
+
     [HttpPost("MedicineTakenList")]
     public async Task<IActionResult> MedicineTaken([FromBody] MedicineTimeTakenDto model)
     {
@@ -30,14 +36,18 @@ public class MedicineController : BaseController
             .FindBy(x => model.MedicineTimeIds.Contains(x.Id))
             .ToListAsync();
         Check.EntityExists(medicineTimes, "Medicine time not found");
+
         foreach (var medicineTime in medicineTimes)
         {
             medicineTime.IsTaken = true;
             _unitOfWork.Repository<MedicationTime>().Update(medicineTime);
-
         }
+
+        await _userService.UpdateUserTargetAsync(model.userId, UserTargetTypeEnum.Medicine,
+            (short) medicineTimes.Count);
+
         await _unitOfWork.SaveChangesAsync();
-        
+
         return Ok(new {IsSuccess = true, Message = "Medicine taken"});
     }
 
--- Neuro.Api/Models/MedicineTimeTakenDto.cs
+++ Neuro.Api/Models/MedicineTimeTakenDto.cs
@@ -3,4 +3,5 @@ namespace Neuro.Api.Models;
 public class MedicineTimeTakenDto
 {
     public List<int> MedicineTimeIds { get; set; }
+    public int userId { get; set; }
 }
